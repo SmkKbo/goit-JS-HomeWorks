6704e6d8054e3767977c43eac8b9bfc3639ad387
SmkKbo <BogdanKurilko@ukr.net>
2020-04-17 19:20:42 +0300

    pass

---
+++ goit-JS-HomeWork-2/JS/task7.js
@@ -0,0 +1,37 @@
+//ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ
+const passwords = ["123123123", "111", "qwerty", "qazwsx"];
+let attempts = 3;
+let cancel = 0;
+
+do {
+  const userPassword = prompt("Введите пароль");
+
+  if (userPassword === null) {
+    alert("Прервано пользователем");
+    break;
+  } else {
+    for (const item of passwords) {
+      if (userPassword === item && attempts <= 3) {
+        alert("Добро пожаловать!");
+        cancel = 1;
+        break;
+      } else {
+        attempts -= 1;
+        if (attempts === 0) {
+          break;
+        }
+        alert(`Неверный пароль, у вас осталось ${attempts} попыток`);
+        break;
+      }
+    }
+  }
+
+  if (attempts === 0) {
+    alert("У вас закончились попытки, аккаунт заблокирован!");
+    break;
+  }
+
+  if (cancel === 1) {
+    break;
+  }
+} while (true);
