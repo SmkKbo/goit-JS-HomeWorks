7f7793ec347ff0983d09eeb92624c89bfcb47227
SmkKbo <BogdanKurilko@ukr.net>
2020-04-20 19:14:00 +0300

    task1

---
+++ goit-JS-HomeWork-3/JS/task1.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Задание 1
+
+//Напиши скрипт, который, для объекта user, последовательно:
+
+//добавляет поле mood со значением 'happy'
+//заменяет значение hobby на 'skydiving'
+//заменяет значение premium на false
+//выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+
+const user = {
+    name: "Mango",
+    age: 20,
+    hobby: "html",
+    premium: true
+  };
+  
+  user.mood = "happy";
+  user.hobby = "skydiving";
+  user.premium = "false";
+  
+  for (const key in user) {
+    if (user.hasOwnProperty(key)) {
+      const element = user[key];
+      console.log(`${key}: ${element}`);
+    }
+  }
+  
